e81ecec820ff2a455b086fafefe76a1a6ff701e5
Nandan <nandanmarkrao@gmail.com>
2019-03-29 21:06:15 +0100

    lots of disks

--- setup-kube.sh
+++ setup-kube.sh
@@ -8,22 +8,23 @@ PROJECT=toixotoixo
 CLUSTER_NAME=toixo-cluster
 STATIC_IP=34.76.183.123
 
+# gcloud container clusters create my-regional-cluster --num-nodes 2 --region europe-west1 \
+# --node-locations europe-west1-b,europe-west1-c
+
 gcloud --project $PROJECT container clusters get-credentials --region europe-west1 $CLUSTER_NAME
 
 # Setup Tiller with proper RBAC service account
 kubectl create -f rbac-config.yaml
 helm init --service-account tiller
 
-# This is necessary for NGINX INGRESS -- maybe not when installing via helm?
+# This is necessary for NGINX INGRESS (maybe?) and LOCAL SSD PROVISIONER
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
 
 # Install nginx-ingress
 helm install stable/nginx-ingress --name nginx-ingress --set rbac.create=true --set controller.service.loadBalancerIP=$STATIC_IP
 
 # Setup cert-manager
-helm install \
-    --name cert-manager \
-    stable/cert-manager
+helm install --name cert-manager stable/cert-manager
 
 # Create cluster-issuer
 kubectl create -f cm-issuer.yaml
